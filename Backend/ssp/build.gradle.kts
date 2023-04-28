c60ee65fc0d404fda8ca22bf3934da68e89d4068
danitherex <daniel@maurer-koblenz.de>
2023-04-28 15:29:18 +0200

    Initialized Springboot Project

---
+++ Backend/ssp/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.6"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.dhbw.unternehmenb"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/SoftwareEngineeringUnternehmenBApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.unternehmenb.ssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringUnternehmenBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringUnternehmenBApplication.class, args);
+	}
+
+}
