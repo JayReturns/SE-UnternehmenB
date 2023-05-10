40ddf117f7e75168d6483dcd700f0fe8f3f10f20
BerniProgrammer <95533188+BerniProgrammer@users.noreply.github.com>
2023-05-10 19:33:32 +0200

    added api response annotations

--- Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/interfaces/ServerApi.java
+++ Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/interfaces/ServerApi.java
@@ -4,13 +4,14 @@ import com.dhbw.unternehmenb.ssp.model.Role;
 import com.dhbw.unternehmenb.ssp.model.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/v1")
 @SecurityScheme(
@@ -23,12 +24,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ServerApi {
     @GetMapping("user")
     @Operation(summary = "Get User Data")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Daten gesendet", content = @Content(mediaType = "application/json",schema = @Schema(implementation = User.class)))
+    })
     ResponseEntity<User> getUserData() throws Exception;
     //we may want to use Response/Request-bodies for more data
 
 
     @PostMapping("user")
     @Operation(summary = "Register User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Daten gesendet", content = @Content)
+    })
     ResponseEntity<String> createUser(
             @RequestParam String name,
             @RequestParam String lastname,
