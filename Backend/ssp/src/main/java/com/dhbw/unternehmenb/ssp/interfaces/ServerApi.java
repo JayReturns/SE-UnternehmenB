ac619dbf6cf1685fd530b21d1dc0d9b18b4c2e38
BerniProgrammer <95533188+BerniProgrammer@users.noreply.github.com>
2023-05-10 19:21:10 +0200

    fixed #51 moved user fetch, removed unnecessarily repeated firebase-call

--- Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/interfaces/ServerApi.java
+++ Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/interfaces/ServerApi.java
@@ -3,21 +3,33 @@ package com.dhbw.unternehmenb.ssp.interfaces;
 import com.dhbw.unternehmenb.ssp.model.Role;
 import com.dhbw.unternehmenb.ssp.model.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("api/v1")
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+@SecurityRequirement(name = "Bearer Authentication")
 public interface ServerApi {
     @GetMapping("user")
     @Operation(summary = "Get User Data")
-    ResponseEntity<User> getUserData(@RequestHeader("Authorization") String auth) throws Exception;
+    ResponseEntity<User> getUserData() throws Exception;
     //we may want to use Response/Request-bodies for more data
 
 
     @PostMapping("user")
     @Operation(summary = "Register User")
     ResponseEntity<String> createUser(
-            @RequestHeader("Authorization") String auth,
             @RequestParam String name,
             @RequestParam String lastname,
             @RequestParam Role role,
