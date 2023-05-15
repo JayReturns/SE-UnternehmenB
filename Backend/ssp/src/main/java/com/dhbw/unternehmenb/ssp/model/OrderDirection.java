0f5a3f03231211f1e4ce7cb72a19735968468943
cherd31 <jarmar416@gmail.com>
2023-05-15 18:14:14 +0200

    deleted unnecessary files

--- Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/model/OrderDirection.java
+++
@@ -1,5 +0,0 @@
-package com.dhbw.unternehmenb.ssp.model;
-
-public enum OrderDirection {
-    ASC, DESC
-}
--- Backend/ssp/src/main/java/com/dhbw/unternehmenb/ssp/model/OrderParameter.java
+++
@@ -1,5 +0,0 @@
-package com.dhbw.unternehmenb.ssp.model;
-
-public enum OrderParameter {
-    VACATION_START, STATUS
-}
