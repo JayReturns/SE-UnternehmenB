60a105a5a7efd4cb31c49326a584bdc4e1a3a7a6
Ynfiniti <sean_schwarz@gmx.de>
2023-05-21 18:29:12 +0200

    Include Interface for vacations grouped by users

--- Frontend/src/app/models/vacation.model.ts
+++ Frontend/src/app/models/vacation.model.ts
@@ -15,6 +15,15 @@ export interface Vacation {
   rejectReason?: string;
 }
 
+/**
+ * Names must match fields from json response
+ * refer http://localhost:8080/swagger-ui/index.html#/ -> Schemas -> AllUsersVRResponseBody
+ */
+export interface GroupedVacation {
+  user: User
+  requests: Vacation[]
+}
+
 export enum Status {
   REQUESTED = "REQUESTED",
   APPROVED = "APPROVED",
