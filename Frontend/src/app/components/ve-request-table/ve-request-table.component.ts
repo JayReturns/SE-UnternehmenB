d7aa7feab547a56e33d1bb842cb778877ef49a17
Ynfiniti <sean_schwarz@gmx.de>
2023-06-02 12:14:05 +0200

    create component

---
+++ Frontend/src/app/components/ve-request-table/ve-request-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ve-request-table',
+  templateUrl: './ve-request-table.component.html',
+  styleUrls: ['./ve-request-table.component.scss']
+})
+export class VeRequestTableComponent {
+
+}
